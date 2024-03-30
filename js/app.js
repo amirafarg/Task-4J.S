5e58dc1b14769d601ba510677821c16b686e55bc
amirafarag <amiraazam301@gmail.com>
2024-03-30 16:29:15 +0200

    adding-thenews

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
-var studentNames = ["asmaa", "mariam", "israa", "amira"];
-var scores = [85, 99, 75, 50];
+// var studentNames = ["asmaa", "mariam", "israa", "amira"];
+// var scores = [85, 99, 75, 50];
+
+var students = [
+{ name: "asmaa", score: "85"},
+{ name: "mariam", score: "99"},
+{ name: "israa", score: "75"},
+{ name: "amira", score: "50"},
+];
 
 var studentToFind = prompt("Enter a student's name:");
  
 var found = false;
 var studentScore;
 
-for (var index = 0; index < studentNames.length; index +=1) {
-    console.log(studentNames[index]);
+for (var index = 0; index < students.length; index +=1) {
+    console.log(students[index].name);
 
-    if (studentNames[index] === studentToFind) {
+    if (students[index].name === studentToFind) {
         found = true;
-        studentScore = scores[index];
+        studentScore = students[index].score;
 
         break;
     }
